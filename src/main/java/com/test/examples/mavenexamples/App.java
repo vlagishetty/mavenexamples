d14b6a4ea14eb268bd238eac616e8d8e54ee0222
vlagishe <vineel.lagishetty@kirklands.com>
2021-08-02 01:34:43 -0500

    initial commit

---
+++ src/main/java/com/test/examples/mavenexamples/App.java
@@ -0,0 +1,14 @@
+package com.test.examples.mavenexamples;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
